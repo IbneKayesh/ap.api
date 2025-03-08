97a72eb747700bea2677c633cd20ee52c0bcc9d1
kayesh <kayesh@IT-KAYESH>
2025-03-08 15:38:42 +0600

    ui project

---
+++ app-client/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import reactLogo from "@/assets/react.svg"; // Use absolute path
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img className="size-12 shrink-0" src={reactLogo} alt="ChitChat Logo" />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            ChitChat
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
